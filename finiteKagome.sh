577fde52de5ba4b72ed0a20c2f00bd4160e4320a
Tateki Obori <obori@reims.issp.u-tokyo.ac.jp>
2021-04-28 11:45:55 +0900

    update

---
+++ finiteKagome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p gr6
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+
+##### Example of output files
+# #SBATCH -J single   # Job name
+# #SBATCH -o %x.o%J   # Standard output
+# #SBATCH -e %x.e%J   # Standard error
+
+python3 ./src/finite_kagome.py --D 20 --beta_end 1000.0 --dt 0.001 --instate HeisenD20 --Hx 0.50
